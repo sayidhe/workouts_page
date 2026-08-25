10a9a5c9bec820bccf6fbc630968d21b461b8aed
sayidhe <herq12315@gmail.com>
2026-08-25 22:58:55 +0800

    update the heartrate

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -41,7 +41,7 @@ const ROAD_LABEL_DISPLAY = true;
 const PRIVACY_MODE = false;
 // updated on 2024/11/17: lights are turned off by default
 //set to `false` if you want to make light off as default, only effect when `PRIVACY_MODE` = false
-const LIGHTS_ON = false;
+const LIGHTS_ON = true;
 //set to `true` if you want to show the 'Elevation Gain' column
 const SHOW_ELEVATION_GAIN = true;
 // richer title for the activity types (like garmin style)
